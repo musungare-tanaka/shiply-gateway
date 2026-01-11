72ce708436f12108eff542a1c86f5635d8a8cab2
Tanaka878 <musungaretanaka@gmail.com>
2026-01-11 13:40:46 +0200

    Bootstrapping the project

---
+++ src/main/java/com/tanaka/shiply/ShiplyApplication.java
@@ -0,0 +1,13 @@
+package com.tanaka.shiply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanaka/shiply/ShiplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanaka.shiply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
