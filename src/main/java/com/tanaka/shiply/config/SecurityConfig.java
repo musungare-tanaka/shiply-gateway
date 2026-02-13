b248a6c84d2d36f03d3c0c041055f6b999125cd2
Tanaka878 <musungaretanaka@gmail.com>
2026-02-13 18:52:17 +0200

    Adding the docker files and the configuration files to be ready for deployment

--- src/main/java/com/tanaka/shiply/config/SecurityConfig.java
+++ src/main/java/com/tanaka/shiply/config/SecurityConfig.java
@@ -2,9 +2,8 @@ package com.tanaka.shiply.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -13,22 +12,13 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .pathMatchers("/auth/**").permitAll()
-                        .anyExchange().authenticated()
-                )
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+                        .anyExchange().permitAll()
+                );
 
         return http.build();
-    }
-
-    @Bean
-    public ReactiveJwtDecoder reactiveJwtDecoder() {
-        System.out.println("Decoding urls");
-        // Replace this URL with the JWKS endpoint of your OAuth2 provider / User Management service
-        String jwkSetUri = "https://your-auth-server/.well-known/jwks.json";
-
-        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
 }
